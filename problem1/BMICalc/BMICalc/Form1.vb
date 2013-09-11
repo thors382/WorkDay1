750fb0e7dd1da1b35a13162d17d2acae9e25f7b4
Mark Thorson <thors382@umn.edu>
2013-09-11 15:23:54 -0500

    ffasdfasd

--- problem1/BMICalc/BMICalc/Form1.vb
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -3,6 +3,31 @@
 Public Class Form1
 
     Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
+        Dim hSt As String
+        Dim wSt As String
+
+        hSt = txtHeight.Text
+        wSt = txtWeight.Text
+
+        Dim h As Double
+        Dim w As Double
+
+        h = CDbl(hSt)
+        w = CDbl(wSt)
+
+        Dim BMI As Double
+        BMI = (w * 703) / (h^ 2)
+
+
+        TextBox1.Text = CStr(BMI)
+
+    End Sub
+
+    Private Sub txtWeight_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtWeight.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
